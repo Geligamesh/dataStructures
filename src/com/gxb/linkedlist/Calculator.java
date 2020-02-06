17eace6c3e8792ce28cca2e264969b05440fad34
Geligamesh <535758155@qq.com>
2020-02-06 23:55:06 +0800

    数据结构与算法学习

--- src/com/gxb/linkedlist/Calculator.java
+++ src/com/gxb/linkedlist/Calculator.java
@@ -1,6 +1,58 @@
 package com.gxb.linkedlist;
 
 public class Calculator {
+
+    public static void main(String[] args) {
+
+        String expression = "7+2*6-4";
+        ArrayStack2 numStack = new ArrayStack2(10);
+        ArrayStack2 operStack = new ArrayStack2(10);
+
+        int num1;
+        int num2;
+        int oper;
+        int result;
+        int index = 0;
+        char ch;
+
+        ch = expression.substring(index, index + 1).charAt(0);
+        while (true) {
+            if (operStack.isOper(ch)) {
+                if (operStack.isEmpty()) {
+                    if (operStack.priority(ch) <= operStack.priority(operStack.peek())) {
+                        num1 = numStack.pop();
+                        num2 = numStack.pop();
+                        oper = operStack.pop();
+                        result = operStack.cal(num1, num2, oper);
+                        numStack.push(result);
+                        operStack.push(ch);
+                    }else {
+                        operStack.push(ch);
+                    }
+                }else {
+                    operStack.push(ch);
+                }
+            }else {
+                numStack.push(ch - 48);
+            }
+            index++;
+            if (index == expression.length()) {
+                break;
+            }
+        }
+
+        while (true) {
+            if (operStack.isEmpty()) {
+                break;
+            }
+            num1 = numStack.pop();
+            num2 = numStack.pop();
+            oper = operStack.pop();
+            result = operStack.cal(num1, num2, oper);
+            numStack.push(result);
+        }
+        System.out.println("result=" + numStack.pop());
+    }
 }
 
 class ArrayStack2 {
@@ -12,6 +64,50 @@ class ArrayStack2 {
     public ArrayStack2(int maxSize) {
         this.maxSize = maxSize;
         stack = new int[maxSize];
+    }
+
+    public int cal(int num1,int num2,int oper) {
+        int result = 0;
+        switch (oper) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+
+    //判断是不是一个运算符
+    public boolean isOper(int oper) {
+        return oper == '+' || oper == '-' || oper == '*' || oper == '/';
+    }
+
+    //返回运算符的优先级，优先级使用数字表示
+    //数字越大，表示优先级强高
+    public int priority(int oper) {
+        if (oper == '*' || oper == '/') {
+            return 1;
+        }else if (oper == '+' || oper == '-') {
+            return 0;
+        }else {
+            //假定目前的表达式只有+、-、*、/
+            return -1;
+        }
+    }
+
+    //增加一个方法，可以返回当前栈顶的值，但是不是真正的pop
+    public int peek() {
+        return stack[top];
     }
 
     //是否栈满
